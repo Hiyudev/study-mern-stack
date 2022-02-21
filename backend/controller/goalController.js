a279d611cb70c8cb971f2f6a99a42c24c8443d66
Yuki2dev <KeysHD@users.noreply.github.com>
2022-02-21 11:30:36 -0300

    ✅ Added: Goal with user authentication

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 /**
  * Get goals
@@ -8,7 +9,7 @@ const Goal = require("../models/goalModel");
  * @access private
  */
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
 
   res.status(200).json(goals);
 });
@@ -25,6 +26,7 @@ const setGoal = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json(goal);
@@ -41,6 +43,17 @@ const updateGoal = asyncHandler(async (req, res) => {
     res.status(400);
 
     throw new Error("Goal not found");
+  }
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not found");
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -61,6 +74,18 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
 
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not found");
   }
 
   await goal.remove();
