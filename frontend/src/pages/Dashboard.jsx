d577b49ac405d05f9c29514db815df8728e1b7e5
Yuki2dev <KeysHD@users.noreply.github.com>
2022-02-25 20:08:11 -0300

    ✅ Added: Strict Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,19 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 function DashboardPage() {
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login")
+    }
+  }, [user, navigate])
+
   return (
     <div>Dashboard page</div>
   );
