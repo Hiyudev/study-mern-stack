0ed79ecb1ebc33f693fbca578b12a7c7a588dfa5
Yuki2dev <KeysHD@users.noreply.github.com>
2022-02-28 09:30:49 -0300

    ✅ Added: Working dashboard page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,21 +1,56 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import GoalForm from '../components/GoalForm';
+import GoalItem from '../components/GoalItem';
+import Spinner from '../components/Spinner';
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function DashboardPage() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading } = useSelector((state) => state.goal);
 
   useEffect(() => {
     if (!user) {
       navigate("/login")
     }
-  }, [user, navigate])
+
+    dispatch(getGoals());
+
+    return () => {
+      reset();
+    }
+  }, [user, navigate, dispatch])
+
+  if (isLoading) {
+    return (
+      <Spinner />
+    )
+  }
 
   return (
-    <div>Dashboard page</div>
+    <section className='heading'>
+      <h1>Welcome {user ? user.name : "User"}</h1>
+      <p>Goals dashboard</p>
+
+      <GoalForm />
+
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((g) =>
+              <GoalItem key={g._id} goal={g} />
+            )}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
+    </section>
   );
 }
 
